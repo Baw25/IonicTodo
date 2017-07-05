ac42c7b5499f1cb52b6748d38009bad1116a280d
Blake Wills <blake.wills525@gmail.com>
2017-07-05 15:27:42 -0700

    First render of list items

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public items;
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  ionViewDidLoad(){
+
+    this.items = [
+      {title: "Take Out Trash", description: "Take out the two bags of trash"},
+      {title: "Call Mom", description: "Talk to mom about interesting things"},
+      {title: "Feed the dogs", description: "Take the dogs out to a steak dinner"},
+      {title: "Balance Budget", description: "Do some stuff with my spreadsheets"},
+      {title: "Buy groceries", description: "Buy fruits and veggies"}
+     ];
+
+  }
+
+  addItem(){
+
+  }
+
+  viewItem(){
+
+  }
+
 
 }
